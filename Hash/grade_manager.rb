536ce8c67a90d7b347eca43e42254ef8d2fd3a03
kristinadd <kristina@productboard.com>
2025-04-18 13:19:58 +0200

    Article Prep

--- Hash/grade_manager.rb
+++ Hash/grade_manager.rb
@@ -2,35 +2,27 @@ class GradeManager
   attr_reader :students  # This makes @students readable from outside the class
 
   def initialize
-    @students = {}  # This will store all student data
+    @students = {}
   end
 
   def add_student(name, grades = {})
     @students[name] = grades
   end
 
-  # 2. Add or update a grade for a specific student and subject
   def update_grade(name, subject, grade)
-    # Implement this method
+    @students[name][subject] = grade
   end
 
-  # 3. Get a student's average grade
+
   def student_average(name)
-    # Implement this method
   end
 
-  # 4. Get the class average for a specific subject
   def subject_average(subject)
-    # Implement this method
   end
 
-  # 5. Get the highest grade in a specific subject
   def highest_grade(subject)
-    # Implement this method
   end
 
-  # 6. Get a list of students who scored above a certain grade in a subject
   def students_above_grade(subject, minimum_grade)
-    # Implement this method
   end
 end
--- Hash/test.rb
+++ Hash/test.rb
@@ -16,4 +16,42 @@ class Test
 
   manager.add_student('Kristina', grades)
   puts manager.students
+  puts manager
+  puts manager.inspect
+
+  puts manager.students['Kristina']
+  puts manager.students['Kristina'][:math]
+
+  grades_bobby = Hash.new
+  grades_bobby[:math] = 19
+  grades_bobby[:science] = 23
+  grades_bobby[:history] = 43
+  grades_bobby[:english] = 32
+  grades_bobby[:spanish] = 12
+  grades_bobby[:german] = 0
+  grades_bobby[:french] = 5
+
+  manager.add_student('Bobby', grades_bobby)
+
+  students = manager.students
+
+  puts students["Bobby"]
+
+  puts students.class
+
+  gradeB = 200
+  manager.update_grade("Bobby", :math, gradeB)
+  puts students["Bobby"]
+
+  gradeK = 300
+  manager.update_grade("Kristina", :math, gradeK)
+  puts students["Kristina"]
+
+  puts students["Kristina"][:math].object_id
+  puts students["Bobby"][:math].object_id
+
+  puts students["Kristina"][:math].object_id == students["Bobby"][:math].object_id
+
+  puts :math.object_id == :math.object_id
+  puts students["Kristina"].keys.include?(:math) && students["Bobby"].keys.include?(:math)
 end
